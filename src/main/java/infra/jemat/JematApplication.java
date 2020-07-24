f793e2907dd246ddae5a429cc16e3fb3a6845043
deocksoo <deocks@woowahan.com>
2020-07-24 21:37:25 +0900

    initial commit

---
+++ src/main/java/infra/jemat/JematApplication.java
@@ -0,0 +1,13 @@
+package infra.jemat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JematApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JematApplication.class, args);
+    }
+
+}
---
+++ src/test/java/infra/jemat/JematApplicationTests.java
@@ -0,0 +1,13 @@
+package infra.jemat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JematApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
