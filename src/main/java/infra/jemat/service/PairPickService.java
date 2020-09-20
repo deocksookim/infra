2b56b6c5c20ac7d32b5a7cd5e03cb0fc7e4b4445
deocksoo <deocks@woowahan.com>
2020-09-20 18:31:41 +0900

    gc 발생 코드 추가

--- src/main/java/infra/jemat/service/PairPickService.java
+++ src/main/java/infra/jemat/service/PairPickService.java
@@ -5,6 +5,8 @@ import infra.jemat.domain.Pair;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 @RequiredArgsConstructor
 public class PairPickService {
@@ -12,6 +14,15 @@ public class PairPickService {
 
     public String getPair() {
         new Pair("끝장페어", "010xxxxxxxx", 99);
+        randomlyTriggerFullGc();
         return pairRepository.pickRandomPair();
+    }
+
+    private void randomlyTriggerFullGc() {
+        Random random = new Random();
+        int randomPickNumber = random.nextInt(9);
+        if (randomPickNumber < 2) {
+            System.gc();
+        }
     }
 }
